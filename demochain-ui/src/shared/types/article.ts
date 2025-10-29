aa32c1249a0feda9cbdc5e8760c80f1215f8a328
smailsnail <651023907@qq.com>
2025-10-29 21:46:39 +0800

    修复注册登录功能

--- demochain-ui/src/shared/types/article.ts
+++ demochain-ui/src/shared/types/article.ts
@@ -1,21 +1,3 @@
-interface Article {
-    id: string;
-    title: string;
-    excerpt: string;
-    tags: string[];
-    slug: string;
-    views: number;
-    published: string;
-}
-
-export interface ArticleListResponse {
-    articles: Article[];
-    total: number;
-    page: number;
-    pageSize: number;
-    hasMore: boolean;
-}
-
 export interface ArticleCategory {
     id: string;
     name: string;
