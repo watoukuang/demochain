f24c98c6f4396d9362088ec3b5eed53fe64b62fa
smailsnail <651023907@qq.com>
2025-10-30 19:40:44 +0800

    调整响应式

--- demochain-ui/src/views/mobile/index.tsx
+++ demochain-ui/src/views/mobile/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Link from 'next/link';
 import Articles from "@/components/Articles";
-import Logo from "@/components/Logo";
 
 export default function MobileHero(): React.ReactElement {
     return (
